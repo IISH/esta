295f61d32b2c83b43ef81c70bdbbbd7b50276da1
rlzijdeman <richard.zijdeman@iisg.nl>
2020-05-18 20:59:01 +0200

    added missing coordinates

---
+++ harbour-info-clean.R
@@ -0,0 +1,27 @@
+# Purpose: read in excel file and save as .csv
+# Date: March 6th, 2020
+
+# clean workspace
+rm(list=ls())
+
+# load libs
+library(tidyverse)
+library(readxl)
+library(uuid)
+
+#set working dir
+setwd("~/git/esta/")
+
+# read in data
+df <- read_excel("./data/source/harbour-info.xlsx")
+
+# check data
+df <- df[,c(1:3,5)]
+
+# generate uuid
+df$locRepId <- UUIDgenerate(n=dim(df)[1])
+
+# save file as .csv
+write.csv(df, "./data/derived/harbour-info.csv", row.names = FALSE)
+
+#EOF
--- scripts/voyage-data-xlsx2csv.R
+++ scripts/voyage-data-xlsx2csv.R
@@ -13,8 +13,13 @@ df <- read_excel("./data/source/2020-02-11%20Voyage%20data%20for%20visualisation
 # names(df)
 
 # remove capitals from header
-names(df) <- tolower(names(df))
+names(df) <- str_to_lower(names(df))
 df
+
+# create year variable
+df <- df %>%
+  mutate(departure_year = str_sub(departure_date, start = -4),
+         arrival_year = str_sub(arrival_date, start = -4 )  )
 
 # save file as .csv
 write.csv(df, "./data/derived/esta-demo.csv", row.names = FALSE)
